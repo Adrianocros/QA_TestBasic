2c8bf363bbb28df9a8b8aaacdf8da711377fe1f2
Adriano Bianchi Crosoletto <adrianobianchi@outlook.com>
2023-06-29 16:21:46 -0300

    Ajustando para verificar resumo mensal

--- projetobascic/src/main/java/br/ce/adrianobasetres/ContaTest/ResumoTest.java
+++ projetobascic/src/main/java/br/ce/adrianobasetres/ContaTest/ResumoTest.java
@@ -7,6 +7,10 @@ import org.junit.Assert;
 import org.junit.FixMethodOrder;
 import org.junit.Test;
 import org.junit.runners.MethodSorters;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+import java.util.List;
 
 import static br.ce.adrianobasetres.core.DriverFactory.getDriver;
 
@@ -16,7 +20,7 @@ public class ResumoTest extends BaseTest {
     private MenuPage menuPage = new MenuPage();
     private ResumoPage resumoPage = new ResumoPage();
 
-    @Test
+   // @Test
     public void test1_ExluirMovimentacao(){
         menuPage.acessarResumoMensal();
         resumoPage.excluirMovimentacao();
@@ -24,9 +28,21 @@ public class ResumoTest extends BaseTest {
         Assert.assertEquals("Movimentação removida com sucesso!", resumoPage.mensagemSucesso());
     }
 
-    @Test
+    @Test //(expected = NoSuchElementException.class) Gera falso positivo, caso tenha varias linhas ele passa
     public void test2_ResumoMensal(){
         menuPage.acessarResumoMensal();
         Assert.assertEquals("Seu Barriga - Extrato", getDriver().getTitle());
+
+        //verifica as exeção no escopo correto porem busca o elemento depois captura a exeção gera delay
+//        try {
+//            getDriver().findElement(By.xpath("//*[@id='tabelaSaldo']/tbody/tr"));
+//            Assert.fail();
+//        }catch (NoSuchElementException e){
+//        }
+
+        //Forma que não trabalha com a exeção, melhora a performance e faz a busca diretamente na logica.
+      List<WebElement> elementosEncontrados =
+              getDriver().findElements(By.xpath("//*[@id='tabelaSaldo']/tbody/tr"));
+      Assert.assertEquals(0,elementosEncontrados.size());
     }
 }
